119015b63b485bcb8c41675f13450e3ac9082c9e
dev-charles <lucky.goldragon@gmail.com>
2025-01-10 03:14:28 -0500

    add tab into search-query layout and add tab pages

---
+++ src/app/(dashboard)/search-query/failed/page.tsx
@@ -0,0 +1,6 @@
+import { QUERY_STATUS } from "@/types/enums";
+import QueryProcessingView from "@/views/QueryView";
+
+export default function Page() {
+  return <QueryProcessingView status={QUERY_STATUS.FAILED} />;
+}
--- src/app/(dashboard)/search-query/layout.tsx
+++ src/app/(dashboard)/search-query/layout.tsx
@@ -1,15 +1,60 @@
 "use client";
 
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+
 import Toolbar from "@/components/SearchQuery/Toolbar";
 import { QueryFilterContextProvider } from "@/contexts/FilterQueryContext";
 import { QueriesContextProvider } from "@/contexts/QueriesContext";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
+  const pathname = usePathname();
+
+  const tabs = [
+    { id: "all", label: "All", link: "/search-query" },
+    { id: "pending", label: "Pending", link: "/search-query/pending" },
+    {
+      id: "extracting",
+      label: "Extracting",
+      link: "/search-query/extracting",
+    },
+    {
+      id: "extracted",
+      label: "Extracted",
+      link: "/search-query/extracted",
+    },
+    { id: "failed", label: "Failed", link: "/search-query/failed" },
+    { id: "canceled", label: "Canceled", link: "/search-query/canceled" },
+  ];
+
   return (
     <div className="flex-1 flex flex-col text-center p-4 overflow-auto bg-white">
       <QueryFilterContextProvider>
         <QueriesContextProvider>
           <Toolbar />
+          <div className="border-b border-gray-200">
+            <nav className="flex space-x-2 px-4">
+              {tabs.map((tab) => (
+                <>
+                  <Link href={tab.link}>
+                    <button
+                      key={tab.id}
+                      className={`
+                  px-4 py-3 text-sm font-medium border-b-2 transition-colors duration-150
+                  ${
+                    pathname === tab.link
+                      ? "border-blue-500 text-blue-600"
+                      : "border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300"
+                  }
+                `}
+                    >
+                      {tab.label}
+                    </button>
+                  </Link>
+                </>
+              ))}
+            </nav>
+          </div>
           <div className="flex-1 flex flex-col overflow-auto">{children}</div>
         </QueriesContextProvider>
       </QueryFilterContextProvider>
