d5076eebe886a3519404012aca76f769adb66927
dev-charles <lucky.goldragon@gmail.com>
2025-01-16 07:02:39 -0500

    update to initial redirect

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async redirects() {
+        return [
+            {
+                source: '/',
+                destination: '/ingress-video',
+                permanent: true, // Set to true for a permanent redirect (HTTP 308)
+            },
+        ];
+    },
+};
 
 export default nextConfig;
